295f548f1809e00c605497dfbade558a9f0e7b82
jon.mears <jon.mears@leighton.com>
2025-03-04 15:06:54 +0000

    home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,54 +1,43 @@
 import { Flex, Box, Card, Text, Avatar } from '@radix-ui/themes';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <Flex gap="3" direction="column">
+    <Flex gap="3" direction="row" className="p-10">
       <Box width="350px">
-        <Card size="1">
-          <Flex gap="3" align="center">
-            <Avatar size="3" radius="full" fallback="T" color="indigo" />
-            <Box>
-              <Text as="div" size="2" weight="bold">
-                Teodros Girmay
-              </Text>
-              <Text as="div" size="2" color="gray">
-                Engineering
-              </Text>
-            </Box>
-          </Flex>
-        </Card>
+        <Link href="/examples/movies">
+          <Card size="1">
+            <Flex gap="3" align="center">
+              <Avatar size="3" radius="full" fallback="M" color="bronze" />
+              <Box>
+                <Text as="div" size="2" weight="bold">
+                  Movie List
+                </Text>
+                <Text as="div" size="2" color="gray">
+                  List movies from the OMDB API
+                </Text>
+              </Box>
+            </Flex>
+          </Card>
+        </Link>
       </Box>
 
-      <Box width="400px">
-        <Card size="2">
-          <Flex gap="4" align="center">
-            <Avatar size="4" radius="full" fallback="T" color="indigo" />
-            <Box>
-              <Text as="div" weight="bold">
-                Teodros Girmay
-              </Text>
-              <Text as="div" color="gray">
-                Engineering
-              </Text>
-            </Box>
-          </Flex>
-        </Card>
-      </Box>
-
-      <Box width="500px">
-        <Card size="3">
-          <Flex gap="4" align="center">
-            <Avatar size="5" radius="full" fallback="T" color="indigo" />
-            <Box>
-              <Text as="div" size="4" weight="bold">
-                Teodros Girmay
-              </Text>
-              <Text as="div" size="4" color="gray">
-                Engineering
-              </Text>
-            </Box>
-          </Flex>
-        </Card>
+      <Box width="350px">
+        <Link href="/examples/form">
+          <Card size="1">
+            <Flex gap="3" align="center">
+              <Avatar size="3" radius="full" fallback="F" color="crimson" />
+              <Box>
+                <Text as="div" size="2" weight="bold">
+                  Form Example
+                </Text>
+                <Text as="div" size="2" color="gray">
+                  Email and question form
+                </Text>
+              </Box>
+            </Flex>
+          </Card>
+        </Link>
       </Box>
     </Flex>
   );
